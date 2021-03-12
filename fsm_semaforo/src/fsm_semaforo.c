b3eda3835aa444852ba9f4ee8275d6999aeca028
asdobladoUPM <a.sdoblado@alumnos.upm.es>
2021-03-12 14:26:01 +0100

    timer

--- fsm_semaforo/src/fsm_semaforo.c
+++ fsm_semaforo/src/fsm_semaforo.c
@@ -7,7 +7,6 @@
 #include "timer.h"
 #include "client.h"
 
-
 static int espiraObotonP(fsm_t *f)
 {
     fsm_semaforo_t *fp = (fsm_semaforo_t *)f;
@@ -26,7 +25,10 @@ static int espiraObotonP(fsm_t *f)
 static int deadline(fsm_t *f)
 {
     fsm_semaforo_t *fp = (fsm_semaforo_t *)f;
-    return (fp->timer() > fp->deadline);
+    if (fp->timer)
+    {
+        return (fp->timer() > fp->deadline);
+    }
 };
 static int botonSoDeadline(fsm_t *f)
 {
@@ -113,7 +115,7 @@ static fsm_trans_t semaforo_tt[] = {
     {-1, NULL, -1, NULL}};
 
 void fsm_semaforo_init(fsm_semaforo_t *f, fsm_semaforo_espira_func_t espira, fsm_semaforo_botonP_func_t botonP, fsm_semaforo_botonS_func_t botonS,
-fsm_semaforo_timer_func_t timer, int p_verde, int p_amarillo, int p_rojo, int p_peaton, int s_verde, int s_amarillo, int s_rojo, int s_peaton, int deadline)
+                       fsm_semaforo_timer_func_t timer, int p_verde, int p_amarillo, int p_rojo, int p_peaton, int s_verde, int s_amarillo, int s_rojo, int s_peaton, int deadline)
 {
     fsm_init((fsm_t *)f, semaforo_tt);
 
